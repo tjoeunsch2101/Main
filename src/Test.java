b20966d11eb91dc31be506b7477817ec05271560
lew <bit4math@gmail.com>
2024-05-03 10:08:43 +0900

    System.out.println(addNumbers(11, 22));

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test Class");
 		System.out.println("Hello, Test Class");
+
+		System.out.println(addNumbers(11, 22));
 	}
 
 	public static int addNumbers(int number1, int number2) {
