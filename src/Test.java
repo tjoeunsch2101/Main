31d8adf569ae28feb3cc5bcb66b3f0c5e1c3ddaf
lew <bit4math@gmail.com>
2024-05-03 10:07:19 +0900

    20240503 addNumbers

--- src/Test.java
+++ src/Test.java
@@ -4,4 +4,9 @@ public class Test {
 		System.out.println("Test Class");
 		System.out.println("Hello, Test Class");
 	}
+
+	public static int addNumbers(int number1, int number2) {
+		int result = number1 + number2;
+		return result;
+	}
 }
